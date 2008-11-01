611bc42b5895790bb35086b8d7719f6137251568
Justin Knowlden <jaknowlden@gmail.com>
2008-10-31 21:18:18 -0500

    Ensuring spaces don't get removed that should actually exist

--- lib/smurf/stylesheet.rb
+++ lib/smurf/stylesheet.rb
@@ -10,18 +10,24 @@ module Smurf
     def minify(content)
       class << content; include Minifier; end
       content.compress_whitespace.trim_close_bracket.remove_comments.
-        trim(%w[{ , : ;]).seperate_bracket_from_class
+        remove_spaces_outside_block.remove_spaces_inside_block
+        
     end
 
     module Minifier
       def compress_whitespace; gsub!(/\s+/, ' '); end
-      def trim_close_bracket; gsub!(/;? ?\} ?/, '}'); end
+      def trim_close_bracket; gsub!(/;?\s?\}\s?/, '}'); end
       # .*? means it's a non-greedy match
-      def remove_comments; gsub!(/\/\*.*?\*\/ ?/, ''); end
-      def seperate_bracket_from_class; gsub!(/\}\./, "} ."); end
-      def trim(subs)
-        Array(subs).each {|s| gsub!(%r[ ?[#{s}] ?], s)}
-        self
+      def remove_comments; gsub!(/\/\*.*?\*\/\s?/, ''); end
+      # def seperate_bracket_from_class; gsub!(/\}\./, "} ."); end
+      def remove_spaces_outside_block
+        gsub!(/(\A|\})(.*?)\{/) { |m| m.gsub(/(,)\s|\s(?![.#:])/, '\1') }
+      end
+      def remove_spaces_inside_block
+        gsub!(/\{(.*?)\}/) do |m|
+          # remove spaces in the labels/attributes
+          m.gsub(/(?:\A|\s*;)(.*?)(?::\s*|\z)/) { |n| n.gsub(/\s/, '') }
+        end
       end
     end
   end # Stylesheet
