413f47eac9691f5c92ae47b258f5e650577e8c9f
Justin Knowlden <jaknowlden@gmail.com>
2008-10-31 21:43:55 -0500

    Fixed a problem of removing spaces outside of blocks regarding chains of top-level elements

--- lib/smurf/stylesheet.rb
+++ lib/smurf/stylesheet.rb
@@ -9,19 +9,16 @@ module Smurf
     # TODO: deal with string values better (urls, content blocks, etc.)
     def minify(content)
       class << content; include Minifier; end
-      content.compress_whitespace.trim_close_bracket.remove_comments.
-        remove_spaces_outside_block.remove_spaces_inside_block
-        
+      content.compress_whitespace.remove_comments.remove_spaces_outside_block.
+        remove_spaces_inside_block.trim_last_semicolon
     end
 
     module Minifier
       def compress_whitespace; gsub!(/\s+/, ' '); end
-      def trim_close_bracket; gsub!(/;?\s?\}\s?/, '}'); end
       # .*? means it's a non-greedy match
       def remove_comments; gsub!(/\/\*.*?\*\/\s?/, ''); end
-      # def seperate_bracket_from_class; gsub!(/\}\./, "} ."); end
       def remove_spaces_outside_block
-        gsub!(/(\A|\})(.*?)\{/) { |m| m.gsub(/(,)\s|\s(?![.#:])/, '\1') }
+        gsub!(/(\A|\})(.*?)\{/) { |m| m.gsub(/\s?([}{,])\s?/, '\1') }
       end
       def remove_spaces_inside_block
         gsub!(/\{(.*?)\}/) do |m|
@@ -29,6 +26,7 @@ module Smurf
           m.gsub(/(?:\A|\s*;)(.*?)(?::\s*|\z)/) { |n| n.gsub(/\s/, '') }
         end
       end
+      def trim_last_semicolon; gsub!(/;\}/, '}'); end
     end
   end # Stylesheet
 end # Smurf
