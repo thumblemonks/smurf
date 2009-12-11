b4c31b8e04d8dd5c67cda7bc6d5d0819750bd3a4
Justin Knowlden <gus@gusg.us>
2009-12-11 09:40:35 -0600

    We're working with rails 2.3.5 it seems as well. Bumped test rails version in env to prove it

--- test/rails/config/environment.rb
+++ test/rails/config/environment.rb
@@ -1,6 +1,6 @@
 # Specifies gem version of Rails to use when vendor/rails is not present
 unless defined? RAILS_GEM_VERSION
-  RAILS_GEM_VERSION = (ENV['RAILS_GEM_VERSION'] || '2.3.4')
+  RAILS_GEM_VERSION = (ENV['RAILS_GEM_VERSION'] || '2.3.5')
   STDOUT.puts "TESTING with RAILS_GEM_VERSION = #{RAILS_GEM_VERSION}"
 end
 
