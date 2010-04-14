9b6624442f42fbe8b93ec1f630f076eb712bed95
Justin Knowlden <gus@gusg.us>
2010-04-13 21:21:36 -0500

    Minor refactoring of stylesheet test. Remove redundancy

--- test/stylesheet_test.rb
+++ test/stylesheet_test.rb
@@ -2,6 +2,7 @@ require 'test_helper'
 
 context "minifying a non-existent pattern in a stylesheet" do
   setup { Smurf::Stylesheet }
+  helper(:minify) { |content| topic.new(content).minified }
 
   should "want to minify files in the stylesheets directory" do
     topic.minifies?(["a/b/stylesheets/bar.css", "c/d/stylesheets/baz.css"])
@@ -11,30 +12,24 @@ context "minifying a non-existent pattern in a stylesheet" do
     topic.minifies?(["a/b/javascripts/bar.css", "c/d/javascripts/baz.css"])
   end.not!
 
-
-  # Thanks to someone named Niko for finding this
-  should "succeed for removing comments" do
-    topic.new("hi {  mom:  super-awesome; } ").minified
+  should("succeed for removing comments") do
+    minify("hi {  mom:  super-awesome; } ")
   end.equals("hi{mom:super-awesome}")
 
-  should "succeed when no spaces to compress" do
-    topic.new("hi{mom:super-awesome}").minified
+  should("succeed when no spaces to compress") do
+    minify("hi{mom:super-awesome}")
   end.equals("hi{mom:super-awesome}")
 
   # nothing outside, means nothing inside. they are complementary
   should "succeed when no outside or inside blocks" do
-    topic.new("how-did:  this-happen;  typo: maybe;}").minified
+    minify("how-did:  this-happen;  typo: maybe;}")
   end.equals("how-did: this-happen; typo: maybe}")
 
   asserts "when no last semi-colon in block" do
-    topic.new("hi {  mom:  super-awesome } ").minified
+    minify("hi {  mom:  super-awesome } ")
   end.equals("hi{mom:super-awesome}")
 
-  asserts "empty string when no content provided" do
-    topic.new("").minified
-  end.equals("")
+  asserts("empty string when no content provided") { minify("") }.equals("")
 
-  asserts "nil even if nil provided" do
-    topic.new(nil).minified
-  end.nil
+  asserts("nil even if nil provided") { minify(nil) }.nil
 end # minifying a non-existent pattern in a stylesheet
